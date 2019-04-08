be3465515422a7c511ae9ad445e7c7405ca99696
Grace Frank <frankgracey@gmail.com>
2019-04-08 23:54:46 +0100

    added an extra property of pricePerUnit to the food model schema and its Joi validation

--- models/food.js
+++ models/food.js
@@ -4,9 +4,22 @@ const { categoreySchema } = require("./category");
 
 //defining schema for food
 const foodSchema = new mongoose.Schema({
-  name: { type: String, required: true, minlength: 3, maxlength: 50 },
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+    lowercase: true
+  },
   numberInStock: { type: Number, required: true, min: 0, max: 200 },
-  price: { type: Number, required: true, min: 0, max: 200 },
+  pricePerUnit: { type: Number, required: true, min: 0, max: 200 },
+  measurmentUnit: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50,
+    lowercase: true
+  },
   category: { type: categoreySchema, required: true }
 });
 
@@ -23,10 +36,14 @@ function validate(food) {
       .required()
       .min(0)
       .max(200),
-    price: Joi.number()
+    pricePerUnit: Joi.number()
       .required()
       .min(0)
       .max(200),
+    measurmentUnit: Joi.string()
+      .required()
+      .min(1)
+      .max(50),
     categoryId: Joi.string().required()
   };
 
