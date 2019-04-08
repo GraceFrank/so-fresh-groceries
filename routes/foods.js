7362d69412ed1055b1047cc793b3ba673a2016f1
Grace Frank <frankgracey@gmail.com>
2019-04-08 23:55:43 +0100

    added an endpoint to create food in the foods route

--- routes/foods.js
+++ routes/foods.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const { Food, validate } = require("../models/food");
+const { Category } = require("../models/category");
 
+//endpoint to get all foods
 router.get("/", async (req, res) => {
   const foods = await Food.find();
   return res.send(foods);
 });
 
+//endpoint to create food
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const category = await Category.findById(req.body.categoryId);
+  if (!category) return res.status(400).send("invalid category");
+
+  let food = await Food.create({
+    name: req.body.name,
+    numberInStock: req.body.numberInStock,
+    pricePerUnit: req.body.pricePerUnit,
+    measurmentUnit: req.body.measurmentUnit,
+    category: { _id: category._id, name: category.name }
+  });
+
+  return res.send(food);
+});
 module.exports = router;
