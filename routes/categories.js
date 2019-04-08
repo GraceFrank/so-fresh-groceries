e0fc940051c58a6dff46df4e822867fda03e530e
Grace Frank <frankgracey@gmail.com>
2019-04-08 11:46:39 +0100

    added endpoint to update categories

--- routes/categories.js
+++ routes/categories.js
@@ -13,4 +13,19 @@ router.post("/", async (req, res) => {
   const category = await Category.create(req.body);
   return res.send(category);
 });
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const category = await Category.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true }
+  );
+  if (!category) return res.status(404).send("genre does not exist");
+
+  return res.send(category);
+});
+
 module.exports = router;
