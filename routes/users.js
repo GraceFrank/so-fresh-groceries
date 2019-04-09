3fc8ea6e85bb8b38b5bd8d62c8dcf868def853be
Grace Frank <frankgracey@gmail.com>
2019-04-09 12:41:16 +0100

    completed endpoint to create user

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const { User, validate } = require("../models/user");
+const express = require('express');
+const { User, validate } = require('../models/user');
 
 const router = express.Router();
 
 //endpoint to read all users
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const users = await User.find();
   res.send(users);
 });
 
 //endpoint to create user
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   let newUser = await User.create(req.body);
