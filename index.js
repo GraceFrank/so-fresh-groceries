0129238117353adafbcc32fbf900e41ee1d1e762
Grace Frank <frankgracey@gmail.com>
2019-04-09 09:40:44 +0100

    added route handlers for user route

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const categories = require("./routes/categories");
 const foods = require("./routes/foods");
+const users = require("./routes/users");
+
 const app = express();
 
 app.use(express.json());
 app.use("/api/categories", categories);
 app.use("/api/foods", foods);
+app.use("/api/users", users);
 
 //conncet to database
 mongoose
