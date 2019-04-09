764f020e12f86efd50c0e0c282b5ff933100c54c
Grace Frank <frankgracey@gmail.com>
2019-04-09 11:43:35 +0100

    git added endpoint to create user

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ router.get("/", async (req, res) => {
   res.send(users);
 });
 
+//endpoint to create user
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
