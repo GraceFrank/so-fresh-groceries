db42f8047129c4a1ae2e86bbfd81a18fae2e40d8
Grace Frank <frankgracey@gmail.com>
2019-04-11 05:00:59 +0100

    added enpoint for creating an order

--- routes/orders.js
+++ routes/orders.js
@@ -20,4 +20,43 @@ router.get('/:id', async (req, res) => {
   return res.send(order);
 });
 
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const user = await User.findById(req.body.user);
+  if (!user) return res.status(404).send('no such user');
+
+  let food = await Food.findById(req.body.foodItem.foodId);
+  if (!food) return res.status(404).send('no such fooditem');
+  if (req.body.foodItem.quantity > food.numberInStock)
+    return res.status(406).send('cant purchase more than what is in stock');
+
+  const foodItem = {
+    _id: food._id,
+    name: food.name,
+    quantity: req.body.foodItem.quantity,
+    measurmentUnit: food.measurmentUnit,
+    shippingCost: food.shippingCost,
+    pricePerUnit: food.pricePerUnit,
+    totalCost: food.pricePerUnit * req.body.foodItem.quantity
+  };
+
+  const order = new Order({
+    user: { _id: user.id, name: user.name },
+    deliveryAddress: req.body.deliveryAddress,
+    foodItem: foodItem
+  });
+
+  Fawn.Task()
+    .save('orders', order)
+    .update(
+      'foods',
+      { _id: food._id },
+      { $inc: { numberInStock: -order.foodItem.quantity } }
+    )
+    .run();
+
+  return res.send(order);
+});
+
 module.exports = router;
