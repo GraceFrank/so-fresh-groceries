dc7118693168e90f9f0f2e2923daff272e1c5dac
Grace Frank <frankgracey@gmail.com>
2019-04-13 06:00:59 +0100

    added middleware to authorize admins

---
+++ middleware/auth-admin.js
@@ -0,0 +1,5 @@
+// checks if user is an Admin and grants or denies accessa
+module.exports = function authAdmin(req, res, next) {
+  if (!req.user.isAdmin) return res.status(403).send('access denied');
+  next();
+};
