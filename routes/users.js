51358211ec74f6e0b950bfceb9531429c0d896f1
Grace Frank <frankgracey@gmail.com>
2019-04-13 07:43:30 +0100

    updated the endpoint for updating a  user so that only an authorized user can update his account, also updated api validation for user to add an extra validation method for updating a user

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const validate = require('../api-validations/user');
+const { validate, validateUpdate } = require('../api-validations/user');
 const authorize = require('../middleware/authorize');
 const authAdmin = require('../middleware/auth-admin');
 
@@ -35,18 +35,19 @@ router.post('/', async (req, res) => {
   res.send(_.pick(req.body, ['name', 'email', 'address', 'phone']));
 });
 
-//endpoint to get a specific user
+//endpoint to get a specific user, only an admin can view any user
 router.get('/:id', [authorize, authAdmin], async (req, res) => {
   const user = await User.find({ _id: req.params.id });
   if (!user) return res.status(400).send('user does not exist');
   res.send(user);
 });
 
-//endpoint to update a user
-router.put('/:id', async (req, res) => {
-  const { error } = validate(req.body);
+//endpoint to update a user,  a user can update only his account
+router.put('/', authorize, async (req, res) => {
+  const { error } = validateUpdate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+
+  const user = await User.findByIdAndUpdate(req.user._id, req.body, {
     new: true
   });
   if (!user) return res.status(404).send('no such user');
