4eadfb70492fabd53fc9a2a0f3af533e45d610a5
Grace Frank <frankgracey@gmail.com>
2019-04-14 12:13:24 +0100

    added middleware to validate id in request parameters

---
+++ middleware/validateId.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+function validateId(req, res, next) {
+  const validId = mongoose.Types.ObjectId.isValid(req.params.id);
+  if (!validId) return res.status(400).send('invalidId');
+  next();
+}
